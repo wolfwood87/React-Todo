eef8eb8a48627678e0100a70a0a3ef5d1c5da792
Derek <legato_5@yahoo.com>
2019-10-30 18:24:48 -0700

    started todo project with mapping out app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import Todo from './components/TodoComponents/Todo.js';
+import TodoForm from './components/TodoComponents/Todo.js'
+import TodoList from './components/TodoComponents/TodoList.js'
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todo: ''
+    }
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div>
+          <h2>Welcome to your Todo App!</h2>
+          <TodoForm />
+        </div>
+        <TodoList todo={this.state.todo/>
       </div>
     );
   }
